ebb2934cdd4734e7030b49f27565e18257513da2
sandhigarimounika <sandigarimounika05@gmail.com>
2026-05-13 11:24:49 +0530

    DAY-1

---
+++ JAVA-SCRIPT/DAY-1/script.js
@@ -0,0 +1,20 @@
+console.log("HEllo")
+var num1=10;
+var num2=20;
+if (num1>num2){
+    console.log(num1 +" is greater than "+num2)
+}else{
+    console.log(num2 +" is greater than "+num1)
+}
+if(num1%2==0){
+    console.log(num1 +" is even")
+}else{
+    console.log(num1 +" is odd")
+}
+
+var year=2020
+if(year%4==0 & year%100!=0 | year%400==0){
+    console.log(year+" is a leap year")
+}else{
+    console.log(year+" Not a leap year")
+}
